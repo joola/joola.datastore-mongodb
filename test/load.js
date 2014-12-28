20c7f4a68e9d9e822453fd76daa8d56a25048796
Itay Weinberger <itay@joo.la>
2014-12-28 17:36:14 +0200

    #15 fixed some basic fixtures testing.

--- test/load.js
+++ test/load.js
@@ -6,8 +6,8 @@ var
   Provider = require('../lib/index');
 
 describe("Bulk load", function () {
-  var data = [];
   var self = this;
+  var data=[];
   before(function (done) {
     self.provider = new Provider({
       dsn: 'mongodb://localhost:27017/cache'
@@ -21,6 +21,8 @@ describe("Bulk load", function () {
       return done();
     });
   });
+  
+
 
   it("should load a fixtures file", function (done) {
     this.timeout = 99999999999;
@@ -33,29 +35,35 @@ describe("Bulk load", function () {
     }
 
     self.csvConverter.on("end_parsed", function (jsonObj) {
-      done();
+      console.log('done parsing');
+      //done();
     });
 
+    var init = false;
+    var count = 0;
     self.csvConverter.on("record_parsed", function (resultRow, rawRow, rowIndex) {
+      count++;
+      init = true;
       resultRow.timestamp = randomDate(new Date(2013, 0, 1), new Date());
       delete resultRow.hour;
 
       data.push(resultRow);
     });
+
+    var i = 0;
+    var timerID = 0;
+    var push = function () {
+      var _data = data.splice(0, 15000);
+      if (_data.length === 0)
+        return done();
+      self.provider.insert({key: 'demo_kaggle2', storeKey: 'demo_kaggle2'}, _data, {}, function (err) {
+        if (err)
+          return done(err);
+
+        console.log('Saved chunk', ++i);
+        setTimeout(push, 0);
+      });
+    };
+    setTimeout(push, 1000);
   });
-
-  var i = 0;
-  var push = function () {
-    var _data = data.splice(0, 15000);
-    if (!_data)
-      return setTimeout(push, 0);
-    self.provider.insert({key: 'kaggle2', storeKey: 'kaggle2'}, _data, function (err) {
-      if (err)
-        return done(err);
-
-      console.log('Saved chunk', ++i);
-      setTimeout(push, 0);
-    });
-  };
-  setTimeout(push, 1000);
 });
