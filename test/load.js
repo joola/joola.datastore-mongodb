7fb8046339802184742282a0a3d4b1e8fb7c3024
orweinberger <orweinberger@gmail.com>
2014-12-28 17:07:47 +0200

    #15 Minor tweaks to bulk test

--- test/load.js
+++ test/load.js
@@ -6,6 +6,7 @@ var
   Provider = require('../lib/index');
 
 describe("Bulk load", function () {
+  var data = [];
   var self = this;
   before(function (done) {
     self.provider = new Provider({
@@ -22,6 +23,7 @@ describe("Bulk load", function () {
   });
 
   it("should load a fixtures file", function (done) {
+    this.timeout = 99999999999;
     self.fileStream = fs.createReadStream(self.csvFileName);
     self.csvConverter = new Converter({constructResult: false});
     self.fileStream.pipe(self.csvConverter);
@@ -38,10 +40,22 @@ describe("Bulk load", function () {
       resultRow.timestamp = randomDate(new Date(2013, 0, 1), new Date());
       delete resultRow.hour;
 
-      self.provider.insert({key: 'kaggle2', storeKey: 'kaggle2'}, resultRow, function (err) {
-        if (err)
-          return done(err);
-      });
+      data.push(resultRow);
     });
   });
+
+  var i = 0;
+  var push = function () {
+    var _data = data.splice(0, 15000);
+    if (!_data)
+      return setTimeout(push, 0);
+    self.provider.insert({key: 'kaggle2', storeKey: 'kaggle2'}, _data, function (err) {
+      if (err)
+        return done(err);
+
+      console.log('Saved chunk', ++i);
+      setTimeout(push, 0);
+    });
+  };
+  setTimeout(push, 1000);
 });
